a632578991d691f9d377440c2a71162f4b7f8244
Mike Fowler <fowler_mychale@yahoo.com>
2020-05-22 00:26:04 -0700

    added graph

--- filterApp.py
+++ filterApp.py
@@ -26,6 +26,9 @@ df['Start_year'] = df['Start_Date'].dt.year
 df['Start_month'] = df['Start_Date'].dt.month
 df['Start_day'] = df['Start_Date'].dt.day
 
+df['Side'].replace('R',1, inplace=True)
+df['Side'].replace('L',0, inplace=True)
+
 #drop tables < 2017
 #df.drop(df[df['Start_year'] < 2017].index, inplace = True)
 
@@ -34,6 +37,8 @@ df.drop(df[(df['City'] != 'Los Angeles') & (df['City'] != 'Boston') & (df['City'
 
 #drop more unused columns
 df.drop(['Start_Time','End_Time','Start_Date','End_Date'], axis=1,inplace=True)
+
+
 
 # City Graph 
 fig1, ax = plt.subplots(ncols=3,figsize=(15,4))
@@ -54,6 +59,6 @@ fig2.savefig('weather_info', dpi=200)
 plt.close(fig2)
 
 plt.show()
-pd.DataFrame.to_csv(df,"" + time.strftime('%Y-%m-%d') + ".csv",',')
+#pd.DataFrame.to_csv(df,"" + time.strftime('%Y-%m-%d') + ".csv",',')
 
 #https://www.kaggle.com/sobhanmoosavi/us-accidents
--- graph1.py
+++ graph1.py
@@ -7,9 +7,15 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('/Users/Mike_F/Desktop/2020-05-20.csv')
 
+df['Side'].replace('R',1, inplace=True)
+df['Side'].replace('L',0, inplace=True)
+
+fig4, ax = plt.subplots(ncols=1,figsize=(15,4))
+sns.lineplot(x='City',y='Difference',hue='Side', ci=None, data=df).set_title("Graph (Collisions Side and City)")
 
 
-fig, ax = plt.subplots(ncols=3,figsize=(15,4))
+
+fig3, ax = plt.subplots(ncols=3,figsize=(15,4))
 sns.barplot(x='City',y='Difference', data=df,ax=ax[0],hue='Astronomical_Twilight').set_title("Time_Diff (Astronomical_Twilight) ")
 sns.barplot(x='City',y='Difference', data=df, ax=ax[1],hue='Civil_Twilight').set_title("Time_Diff (Civil_Twilight)")
 sns.barplot(x='City',y='Difference', data=df,ax=ax[2],hue='Nautical_Twilight').set_title("Time_Diff (Nautical_Twilight)")
@@ -21,10 +27,13 @@ sns.lineplot(x='Difference', y='Humidity(%)', data=df, ax=axs[1],hue='City',ci=N
 sns.lineplot(x='Difference', y='Pressure(in)', data=df, ax=axs[2],hue='City',ci=None).set_title("Time_DIff (Pressure(in))")
 
 
-fig.savefig('Time_Difference')
-plt.close(fig)
-
-
 fig2.savefig('Time_Difference (by climate)')
 plt.close(fig2)
+
+
+fig3.savefig('Time_Difference')
+plt.close(fig3)
+
+fig4.savefig('Side')
+plt.close(fig4)
 plt.show()
